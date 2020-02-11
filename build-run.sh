601afca1c8fc4467e5ca17ca1680e4f95ed8f8a3
Amal Gamage <amalgamage@Amals-MacBook-Pro.local>
2020-02-11 14:16:28 +0530

    updated backend apis

--- build-run.sh
+++ build-run.sh
@@ -1,16 +1,9 @@
-# Building Front-End
-cd frontend
-npm install
-npm run build
-docker build -t school-listing/frontend .
-docker run -d -p 8100:80 school-listing/frontend
-
-cd ..
-
 # Building Database
 cd database
 docker build -t school-listing/db .
 docker run -d -p 27018:27017 school-listing/db
+
+cd ..
 
 # Building Back-End
 cd backend
@@ -18,3 +11,12 @@ npm install
 npm run build
 docker build -t school-listing/backend .
 docker run -d -p 3000:3000 school-listing/backend
+
+cd .. 
+
+# Building Front-End
+cd frontend
+npm install
+npm run build
+docker build -t school-listing/frontend .
+docker run -d -p 8100:80 school-listing/frontend
